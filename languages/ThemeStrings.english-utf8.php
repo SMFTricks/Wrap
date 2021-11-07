5d0f9ab87d588821787545906ecd9b88680786ee
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-07 17:19:27 -0600

    gh release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/ThemeStrings.english-utf8.php
@@ -0,0 +1,34 @@
+<?php
+// Version: 2.0; ThemeStrings
+
+$txt['icons_check'] = 'Enable social icons';
+$txt['rs_facebook'] = 'Facebook';
+$txt['facebook_check'] = 'Enable icon of Facebook';
+$txt['facebook_text'] = 'URL of the page to Facebook';
+$txt['rs_twitter'] = 'Twitter';
+$txt['twitter_check'] = 'Enable icon of Twitter';
+$txt['twitter_text'] = 'URL of the page to Twitter';
+$txt['rs_youtube'] = 'Youtube';
+$txt['youtube_check'] = 'Enable icon of Youtube';
+$txt['youtube_text'] = 'URL of the page to Youtube';
+$txt['rs_rss'] = 'RSS';
+$txt['rss_check'] = 'Enable icon of RSS';
+$txt['rss_text'] = 'URL of the page to RSS';
+$txt['m_link'] = 'Link 1';
+$txt['m_text'] = 'Text 1';
+$txt['m_link2'] = 'Link 2';
+$txt['m_text2'] = 'Text 2';
+$txt['m_link3'] = 'Link 3';
+$txt['m_text3'] = 'Text 3';
+$txt['m_link4'] = 'Link 4';
+$txt['m_text4'] = 'Text 4';
+$txt['m_link5'] = 'Link 5';
+$txt['m_text5'] = 'Text 5';
+$txt['sh_welcome_to'] = 'Welcome to';
+$txt['sh_desc_own_default'] = 'Add a description for your forum';
+$txt['sh_desc_own'] = 'Add your own description';
+$txt['sh_desc_own_desc'] = 'Here you can add your own description in the index of the forum';
+$txt['sh_list_topics'] = 'List of topics for the forum';
+$txt['sh_welcome_to'] = 'Welcome to';
+$txt['backtotop'] = 'Go to the top';
+?>
---
+++ languages/ThemeStrings.spanish_es.php
@@ -0,0 +1,33 @@
+<?php
+// Version: 2.0; ThemeStrings
+
+$txt['icons_check'] = 'Activar iconos sociales';
+$txt['rs_facebook'] = 'Facebook';
+$txt['facebook_check'] = 'Activar icono de Facebook';
+$txt['facebook_text'] = 'Direccion de la pagina de Facebook';
+$txt['rs_twitter'] = 'Twitter';
+$txt['twitter_check'] = 'Activar icono de Twitter';
+$txt['twitter_text'] = 'Direccion de la pagina de Twitter';
+$txt['rs_youtube'] = 'Youtube';
+$txt['youtube_check'] = 'Activar icono de Youtube';
+$txt['youtube_text'] = 'Direccion de la pagina de  Youtube';
+$txt['rs_rss'] = 'RSS';
+$txt['rss_check'] = 'Activar icono de RSS';
+$txt['rss_text'] = 'Direccion de la pagina de RSS';
+$txt['m_link'] = 'Link 1';
+$txt['m_text'] = 'Text 1';
+$txt['m_link2'] = 'Link 2';
+$txt['m_text2'] = 'Text 2';
+$txt['m_link3'] = 'Link 3';
+$txt['m_text3'] = 'Text 3';
+$txt['m_link4'] = 'Link 4';
+$txt['m_text4'] = 'Text 4';
+$txt['m_link5'] = 'Link 5';
+$txt['m_text5'] = 'Text 5';
+$txt['sh_welcome_to'] = 'Bienvenido a';
+$txt['sh_desc_own_default'] = 'A&ntilde;ada una descripci&oacute;n a su foro.';
+$txt['sh_desc_own'] = 'A&ntilde;ade tu propia una descripci&oacute;n ';
+$txt['sh_desc_own_desc'] = 'Aqu&iacute; puedes a&ntilde;adir tu propia descripci&oacute;n en el índice del foro.';
+$txt['sh_list_topics'] = 'Lista de temas para el foro';
+$txt['backtotop'] = 'Volver arriba';
+?>
